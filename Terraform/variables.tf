50809184167f25111419e53940216d31b58c180e
Abdulfattah Assaad <aasaad@savola.com>
2025-01-01 09:15:19 +0300

    fix terrfarom script

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,12 +1,9 @@
 variable "aws_region" {
-  type        = string
   description = "Choose your AWS Region "
   default = "us-east-1"
 }
 
-variable "ami"{
-
-  type        = string
+variable "ami" {
   description = "AWS Ec2 AMI "
   default =  "ami-01816d07b1128cd2d"
 
@@ -18,8 +15,7 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "instance_type" {
+variable "count" {
   description = "number of ec2"
   default = 2
-  type = int
 }
